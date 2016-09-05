41e5b1fd73b463027507d953e77fda9d025e96e5
loaded02 <ocaptainmycaptain@web.de>
2016-09-06 00:39:31 +0200

    Added Unit and E2E tests using mocha, karma, jasmine, protractor

---
+++ config/env/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by moritz on 05.09.16.
+ */
+module.exports = {
+    db: 'mongodb://localhost:27017/mean-book-test',
+    sessionSecret: 'testSessionSecret'
+};
---
+++ protractor.conf.js
@@ -0,0 +1,6 @@
+/**
+ * Created by moritz on 06.09.16.
+ */
+exports.config = {
+    specs: ['public/*[!lib]*/tests/e2e/*.js']
+}
